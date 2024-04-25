c7fe59372c513f3055114f6c3e576a596890b383
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-04-25 11:32:23 -0300

    Criação do projeto springboot servindo a documentação fornecida pelo docusaurus

---
+++ src/test/java/com/github/marciokleber/springsauros/SpringSaurosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.springsauros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSaurosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
